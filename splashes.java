4f1849dd4826a35afc723869c0afeecdd4ef7c1d
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-05-13 12:11:52 +0500

    Create splashes.java

---
+++ splashes.java
@@ -0,0 +1,20 @@
+String[] splashes = {
+    "Welcome to War Bound!",
+    "Get ready to battle!",
+    "Survive at all costs!",
+    "Explore new worlds!",
+    "Have fun!"
+  "Made by Chat GPT and AR-DEV-1!",
+      "2d,2d,2d!",
+  
+      
+};
+int randomSplash = (int) (Math.random() * splashes.length);
+JLabel splashLabel = new JLabel(splashes[randomSplash]);
+// Generate and display a new splash every 15 seconds
+while (true) {
+    Thread.sleep(10000);
+    int randomSplash = (int) (Math.random() * splashes.length);
+    splashLabel.setText(splashes[randomSplash]);
+}
+
