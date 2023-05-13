16f0b8d5af1ee8563b0ff770267f01b0dc9b9fc1
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-05-13 10:47:18 +0500

    Create horse.java

---
+++ horse.java
@@ -0,0 +1,17 @@
+public class Horse {
+    private int health;
+    private int speed;
+
+    public Horse(int health, int speed) {
+        this.health = health;
+        this.speed = speed;
+    }
+
+    public void feed(String food) {
+        // code to feed the horse and increase its health
+    }
+
+    public void ride() {
+        // code to allow the player to ride the horse
+    }
+}
