9c8c0c8fff6df8859b63e99e4fd4d445e87e7857
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-05-28 17:13:36 +0500

    Delete uA.java
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

--- achivements/uA.java
+++
@@ -1,5 +0,0 @@
-// Example code for unlocking an achievement when the player reaches a certain level
-if (player.getLevel() >= 10) {
-    Achievement level10Achievement = new Achievement("Level 10", "Reach level 10");
-    player.unlockAchievement(level10Achievement);
-}
